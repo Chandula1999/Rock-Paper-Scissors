8c4d01f03580bcc5e5193ac9641cdab3dc8c33d0
Chandula1999 <chandulas1999@gmail.com>
2023-02-08 02:21:01 +0530

    Created a function called game

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ function getComputerChoise(){
         "paper",
         "scissors"
     ];
-    // get random inde value
+    // get random index value
     const randomIndex = Math.floor(Math.random() * arr.length);
 
     // get random item
     const Computerselection = arr[randomIndex];
     return Computerselection;
 }
-console.log(getComputerChoise());
 
 function playRound(playerSelection,Computerselection){
     if((playerSelection=="rock"&&Computerselection=="scissor")||(playerSelection=="scissor"&&Computerselection=="paper")||(playerSelection=="paper"&&Computerselection=="rock")){
@@ -20,7 +19,18 @@ function playRound(playerSelection,Computerselection){
     else if((playerSelection=="scissor"&&Computerselection=="rock")||(playerSelection=="spaper"&&Computerselection=="scissor")||(playerSelection=="rock"&&Computerselection=="paper")){
         return "You Lose!";
     }
+    else if(playerSelection!="rock"||playerSelection!="paper"||playerSelection!="scissor"){
+        return "Enter a Valid Option ";
+    }
     else{
         return "Try Again";
     }
 }
+
+
+function game(){
+    const playerSelection = prompt("What is your choise ").toLowerCase();
+    const Computerselection= getComputerChoise();
+    return (playRound(playerSelection,Computerselection));
+}
+
